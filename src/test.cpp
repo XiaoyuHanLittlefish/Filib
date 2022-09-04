700ef5fecf0bb6910a33a6214d0e4e4655df776d
469714875@qq.com <469714875@qq.com>
2022-09-04 19:54:46 +0800

    common update

--- src/test.cpp
+++ src/test.cpp
@@ -1,67 +1,15 @@
 #include <iostream>
-#include "magnum/fjson/parser.h"
-#include "magnum/fjson/json_object.h"
-#include <fstream>
 
-using namespace fjson;
-
-struct Base
+class A
 {
-    int pp;
-    std::string qq;
+public:
+    virtual void func() = 0;
 
-    START_FROM_JSON
-    pp = from("pp", int);
-    qq = from("qq", std::string);
-    END_FROM_JSON
-
-    START_TO_JSON
-    to("pp") = pp;
-    to("qq") = qq;
-    END_TO_JSON
+private:
+    int *p;
 };
-
-struct Mytest
-{
-    int id;
-    std::string name;
-    Base q;
-
-    START_TO_JSON
-    to_struct("base", q);
-    to("id") = id;
-    to("name") = name;
-    END_TO_JSON
-
-    START_FROM_JSON
-    id = from("id", int);
-    name = from("name", std::string);
-    from_struct("base", q);
-    END_FROM_JSON
-};
-
-void test_class_serialization()
-{
-    Mytest test{.id = 32, .name = "fda"};
-    auto item = Parser::FromJSON<Mytest>(R"({"base":{"pp":0,"qq":""},"id":32,"name":"fda"} )"); // serialization
-    std::cout << Parser::ToJSON(item);                                                          // deserialization
-}
-
-void test_string_parser()
-{
-    {
-        std::ifstream fin("../../../../test/test.json");
-        std::string text((std::istreambuf_iterator<char>(fin)), std::istreambuf_iterator<char>());
-        Parser p;
-        p.init(text);
-        auto q = p.parse();
-        std::ofstream fout("../../../../test/test_out.json");
-        fout << q.to_string();
-    }
-}
 
 int main()
 {
-    test_class_serialization();
-    test_string_parser();
+    std::cout << sizeof(A) << std::endl;
 }
