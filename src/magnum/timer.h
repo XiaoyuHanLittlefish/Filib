c6648b5dc52e6ab22f51e6c62255046c2ebf85fd
469714875@qq.com <469714875@qq.com>
2023-07-30 11:54:10 +0800

    file: 增加timer.h

--- src/magnum/timer.h
+++ src/magnum/timer.h
@@ -1,58 +1,25 @@
-#ifndef MAGNUM_TIMER_H__
-#define MAGNUM_TIMER_H__
+#pragma once
 
-#include <chrono>
+#include <functional>
+#include <thread>
 
-/**
- * @brief 
- * 包含了用于计时的timer类
- */
-
-class timer
-{
+namespace timer {
+class Timer {
 public:
-    using millisecond = int64_t;
+  Timer() : m_active(false), m_period(0), m_repeat(-1) {}
+  Timer(int repeat) : m_active(false), m_period(0), m_repeat(repeat) {}
+  ~Timer() {}
 
-public:
-    timer();
-    ~timer();
-    void start();
-    millisecond end();
+  template <typename F, typename... Args>
+  void start(int millisecond, F &&func, Args &&...args);
+
+  void stop();
 
 private:
-    bool running;
-    millisecond timestamp;
+  std::thread m_thread;
+  std::atomic<bool> m_active;
+  std::function<void()> m_func;
+  int m_period; // millisecond
+  int m_repeat;
 };
-
-timer::timer() : running(false), timestamp(0)
-{
-}
-
-timer::~timer()
-{
-}
-
-void timer::start()
-{
-    this->running = true;
-
-    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch());
-
-    this->timestamp = (millisecond)ms.count();
-}
-
-timer::millisecond timer::end()
-{
-    if (!this->running) 
-    {
-        return -1;
-    }
-
-    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch());
-
-    return (millisecond)ms.count() - this->timestamp;
-}
-
-#endif //!MAGNUM_TIMER_H__
+} // namespace timer
