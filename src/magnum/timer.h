5283133e177cb1f974dd00a63f871a77940fd22e
469714875@qq.com <469714875@qq.com>
2023-08-02 12:24:26 +0800

    feat: 完成基于线程的timer

--- src/magnum/timer.h
+++ src/magnum/timer.h
@@ -2,24 +2,62 @@
 
 #include <functional>
 #include <thread>
+#include <atomic>
 
 namespace timer {
 class Timer {
 public:
   Timer() : m_active(false), m_period(0), m_repeat(-1) {}
   Timer(int repeat) : m_active(false), m_period(0), m_repeat(repeat) {}
-  ~Timer() {}
+  ~Timer() { stop(); }
 
   template <typename F, typename... Args>
-  void start(int millisecond, F &&func, Args &&...args);
+  void start(int millisecond, F &&func, Args &&...args) {
+    if (m_active.load()) {
+      return;
+    }
 
-  void stop();
+    m_period = millisecond;
+    m_func = std::bind(std::forward<F>(func), std::forward<Args>(args)...);
+    m_active.store(true);
+
+    m_thread = std::thread([&]() {
+      if (m_repeat < 0) {
+        while (m_active.load()) {
+          std::this_thread::sleep_for(std::chrono::milliseconds(m_period));
+
+          if (!m_active.load()) {
+            return;
+          }
+          m_func();
+        }
+      } else {
+        while (m_repeat > 0) {
+          if (!m_active.load()) {
+            return;
+          }
+          std::this_thread::sleep_for(std::chrono::milliseconds(m_period));
+
+          if (!m_active.load()) {
+            return;
+          }
+          m_func();
+
+          --m_repeat;
+        }
+      }
+    });
+
+    m_thread.detach();
+  }
+
+  void stop() { m_active.store(false); }
 
 private:
   std::thread m_thread;
   std::atomic<bool> m_active;
   std::function<void()> m_func;
   int m_period; // millisecond
-  int m_repeat;
+  int m_repeat; // 重复次数，-1表示无限次
 };
 } // namespace timer
--- xmake.lua
+++ xmake.lua
@@ -1,16 +1,21 @@
 add_rules("mode.debug", "mode.release")
 
--- target("fjson")
---     set_kind("static")
---     add_includedirs("src/magnum/fjson", {public = true})
---     add_files("src/magnum/fjson/*.cpp")
+target("fjson")
+    set_kind("static")
+    add_includedirs("src/magnum/fjson", {public = true})
+    add_files("src/magnum/fjson/*.cpp")
 
 target("threadsafe")
     set_kind("static")
     add_includedirs("src/magnum/threadsafe", {public = true})
 
+target("timer")
+    set_kind("static")
+    add_includedirs("src/magnum", {public = true})
+
 target("test")
     set_kind("binary")
     add_files("src/test.cpp")
     -- add_deps("fjson")
-    add_deps("threadsafe")
+    -- add_deps("threadsafe")
+    add_deps("timer")
