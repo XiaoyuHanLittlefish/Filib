b424d447428123282279bf8ed2eeb5b13fe4780a
469714875@qq.com <469714875@qq.com>
2023-10-02 14:47:07 +0800

    add rbtree function erase

--- src/magnum/frbtree.h
+++ src/magnum/frbtree.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 
 namespace frbtree {
 
@@ -26,6 +27,9 @@ public:
         , color(NodeColor::RED)
         , type(NodeType::ROOT) { }
 
+    void set_data(const T &data_) {
+        this->data = data_;
+    }
     T get_data() {
         return this->data;
     }
@@ -53,6 +57,9 @@ public:
 
     NodeColor get_color() {
         return this->color;
+    }
+    void set_color(NodeColor color) {
+        this->color = color;
     }
     void change_color() {
         switch (this->color) {
@@ -109,11 +116,14 @@ private:
 
 template <typename T> class Rbtree {
 public:
+    using Node = RbtreeNode<T>;
+
+public:
     Rbtree()
         : root_node(nullptr) { }
 
     bool find(const T &data) {
-        RbtreeNode<T> *now_ptr = root_node;
+        Node *now_ptr = root_node;
         while (nullptr != now_ptr) {
             if (now_ptr->get_data() == data) {
                 return true;
@@ -128,20 +138,41 @@ public:
 
     void push(const T &data) {
         if (nullptr != this->root_node) {
-            RbtreeNode<T> *node_ptr = new RbtreeNode<T>(data);
+            Node *node_ptr = new RbtreeNode<T>(data);
             if (insert_node(node_ptr)) {
                 insert_update_pushup(node_ptr);
             }
         } else {
             this->root_node = new RbtreeNode<T>(data);
+            this->root_node->set_type(NodeType::ROOT);
             change_node_color(root_node);
         }
     }
 
+    bool erase(const T &data) {
+        Node *erase_ptr = find_node(data);
+        if (nullptr == erase_ptr) {
+            return false;
+        }
+
+        if (2 == child_count(erase_ptr)) {
+            Node *next_ptr = find_next_node(erase_ptr);
+            if (nullptr == next_ptr) {
+                return false;
+            }
+            erase_ptr->set_data(next_ptr->get_data());
+            erase_ptr = next_ptr;
+        }
+
+        remove_node(erase_ptr);
+        return true;
+    }
+
 private:
-    bool insert_node(RbtreeNode<T> *node_ptr) {
-        RbtreeNode<T> *father_node_ptr = this->root_node;
-        RbtreeNode<T> *next_pos        = nullptr;
+    bool insert_node(Node *node_ptr) {
+        Node *father_node_ptr = this->root_node;
+        Node *next_pos        = nullptr;
+
         while (true) {
             if (node_ptr->get_data() == father_node_ptr->get_data()) {
                 return false;
@@ -165,33 +196,41 @@ private:
         return true;
     }
 
-    NodeColor get_node_color(RbtreeNode<T> *node_ptr) {
+    NodeColor get_node_color(Node *node_ptr) {
         if (nullptr == node_ptr) {
             return NodeColor::BLACK;
         }
         return node_ptr->get_color();
     }
 
-    void change_node_color(RbtreeNode<T> *node_ptr) {
+    void change_node_color(Node *node_ptr) {
         if (nullptr != node_ptr) {
             node_ptr->change_color();
         }
     }
 
-    void rotate_left(RbtreeNode<T> *father_node_ptr) {
-        RbtreeNode<T> *right_child_ptr      = father_node_ptr->get_child(NodeType::RIGHT);
-        RbtreeNode<T> *grandfather_node_ptr = father_node_ptr->get_father();
+    void exchange_two_nodes_color(Node *lhs_ptr, Node *rhs_ptr) {
+        if (get_node_color(lhs_ptr) != get_node_color(rhs_ptr)) {
+            change_node_color(lhs_ptr);
+            change_node_color(rhs_ptr);
+        }
+    }
+
+    void rotate_left(Node *father_node_ptr) {
+        Node *right_child_ptr      = father_node_ptr->get_child(NodeType::RIGHT);
+        Node *grandfather_node_ptr = father_node_ptr->get_father();
 
         if (nullptr != grandfather_node_ptr) {
             NodeType type = father_node_ptr->get_type();
             grandfather_node_ptr->set_child(right_child_ptr, type);
             right_child_ptr->set_father(grandfather_node_ptr);
         } else {
+            right_child_ptr->set_father(nullptr);
             this->root_node = right_child_ptr;
             right_child_ptr->set_type(NodeType::ROOT);
         }
 
-        RbtreeNode<T> *left_grandchild_ptr = right_child_ptr->get_child(NodeType::LEFT);
+        Node *left_grandchild_ptr = right_child_ptr->get_child(NodeType::LEFT);
         father_node_ptr->set_child(left_grandchild_ptr, NodeType::RIGHT);
         if (nullptr != left_grandchild_ptr) {
             left_grandchild_ptr->set_father(father_node_ptr);
@@ -201,20 +240,21 @@ private:
         father_node_ptr->set_father(right_child_ptr);
     }
 
-    void rotate_right(RbtreeNode<T> *father_node_ptr) {
-        RbtreeNode<T> *left_child_ptr       = father_node_ptr->get_child(NodeType::LEFT);
-        RbtreeNode<T> *grandfather_node_ptr = father_node_ptr->get_father();
+    void rotate_right(Node *father_node_ptr) {
+        Node *left_child_ptr       = father_node_ptr->get_child(NodeType::LEFT);
+        Node *grandfather_node_ptr = father_node_ptr->get_father();
 
         if (nullptr != grandfather_node_ptr) {
             NodeType type = father_node_ptr->get_type();
             grandfather_node_ptr->set_child(left_child_ptr, type);
             left_child_ptr->set_father(grandfather_node_ptr);
         } else {
+            left_child_ptr->set_father(nullptr);
             this->root_node = left_child_ptr;
             left_child_ptr->set_type(NodeType::ROOT);
         }
 
-        RbtreeNode<T> *right_grandchild_ptr = left_child_ptr->get_child(NodeType::RIGHT);
+        Node *right_grandchild_ptr = left_child_ptr->get_child(NodeType::RIGHT);
         father_node_ptr->set_child(right_grandchild_ptr, NodeType::LEFT);
         if (nullptr != right_grandchild_ptr) {
             right_grandchild_ptr->set_father(father_node_ptr);
@@ -224,16 +264,16 @@ private:
         father_node_ptr->set_father(left_child_ptr);
     }
 
-    void insert_update_pushup(RbtreeNode<T> *node_ptr) {
-        RbtreeNode<T> *father_node_ptr = node_ptr->get_father();
+    void insert_update_pushup(Node *node_ptr) {
+        Node *father_node_ptr = node_ptr->get_father();
         // 父亲结点为黑色，无影响
         if (NodeColor::BLACK == get_node_color(father_node_ptr)) {
             return;
         }
 
         // 父亲结点为红色
-        RbtreeNode<T> *grandfather_node_ptr = father_node_ptr->get_father();
-        RbtreeNode<T> *uncle_node_ptr       = nullptr;
+        Node *grandfather_node_ptr = father_node_ptr->get_father();
+        Node *uncle_node_ptr       = nullptr;
         if (NodeType::LEFT == father_node_ptr->get_type()) {
             uncle_node_ptr = grandfather_node_ptr->get_child(NodeType::RIGHT);
         } else {
@@ -271,7 +311,149 @@ private:
         }
     }
 
+    Node *find_node(const T &data) {
+        Node *now_ptr = root_node;
+        while (nullptr != now_ptr) {
+            if (now_ptr->get_data() == data) {
+                return now_ptr;
+            } else if (data < now_ptr->get_data()) {
+                now_ptr = now_ptr->get_child(NodeType::LEFT);
+            } else {
+                now_ptr = now_ptr->get_child(NodeType::RIGHT);
+            }
+        }
+        return nullptr;
+    }
+
+    int child_count(Node *ptr) {
+        int cnt = 0;
+        if (nullptr != ptr->get_child(NodeType::LEFT)) {
+            cnt++;
+        }
+        if (nullptr != ptr->get_child(NodeType::RIGHT)) {
+            cnt++;
+        }
+        return cnt;
+    }
+
+    Node *find_next_node(Node *ptr) {
+        Node *now_ptr = ptr->get_child(NodeType::RIGHT);
+        if (nullptr == now_ptr) {
+            return nullptr;
+        }
+
+        while (nullptr != now_ptr->get_child(NodeType::LEFT)) {
+            now_ptr = now_ptr->get_child(NodeType::LEFT);
+        }
+        return now_ptr;
+    }
+
+    NodeType get_another_type(NodeType type) {
+        if (NodeType::LEFT == type) {
+            return NodeType::RIGHT;
+        }
+        return NodeType::LEFT;
+    }
+
+    void erase_fixup_red_black(Node *node_ptr) {
+        if (nullptr != node_ptr) {
+            node_ptr->set_color(NodeColor::BLACK);
+        }
+    }
+
+    void erase_fixup_root(Node *node_ptr) {
+        if (nullptr != node_ptr) {
+            node_ptr->set_color(NodeColor::BLACK);
+        }
+    }
+
+    void erase_fixup_black_black(Node *father_ptr, NodeType type) {
+        Node *brother_node_ptr = father_ptr->get_child(get_another_type(type));
+        Node *near_nephew      = brother_node_ptr->get_child(type);
+        Node *dstt_nephew      = brother_node_ptr->get_child(get_another_type(type));
+
+        if (NodeColor::RED == get_node_color(brother_node_ptr)) {
+            if (NodeType::LEFT == type) {
+                rotate_left(father_ptr);
+
+            } else {
+                rotate_right(father_ptr);
+            }
+            exchange_two_nodes_color(father_ptr, brother_node_ptr);
+            erase_fixup_black_black(father_ptr, type);
+        } else if (NodeColor::BLACK == get_node_color(near_nephew) && NodeColor::BLACK == get_node_color(dstt_nephew)) {
+            if (NodeColor::RED == get_node_color(father_ptr)) {
+                exchange_two_nodes_color(father_ptr, brother_node_ptr);
+            } else {
+                brother_node_ptr->set_color(NodeColor::RED);
+                if (NodeType::ROOT != father_ptr->get_type()) {
+                    erase_fixup_black_black(father_ptr->get_father(), father_ptr->get_type());
+                }
+            }
+        } else if (NodeColor::RED == get_node_color(dstt_nephew)) {
+            exchange_two_nodes_color(father_ptr, brother_node_ptr);
+            if (NodeType::LEFT == type) {
+                rotate_left(father_ptr);
+            } else {
+                rotate_right(father_ptr);
+            }
+            if (nullptr != dstt_nephew) {
+                dstt_nephew->set_color(NodeColor::BLACK);
+            }
+        } else {
+            if (NodeType::LEFT == type) {
+                rotate_right(brother_node_ptr);
+            } else {
+                rotate_left(brother_node_ptr);
+            }
+            exchange_two_nodes_color(brother_node_ptr, near_nephew);
+            exchange_two_nodes_color(father_ptr, near_nephew);
+            if (NodeType::LEFT == type) {
+                rotate_left(father_ptr);
+            } else {
+                rotate_right(father_ptr);
+            }
+            if (nullptr != brother_node_ptr) {
+                brother_node_ptr->set_color(NodeColor::BLACK);
+            }
+        }
+    }
+
+    void remove_node(Node *erase_ptr) {
+        NodeType type       = erase_ptr->get_type();
+        Node    *father_ptr = erase_ptr->get_father();
+        Node    *child_ptr  = erase_ptr->get_child(NodeType::LEFT);
+        if (nullptr == child_ptr) {
+            child_ptr = erase_ptr->get_child(NodeType::RIGHT);
+        }
+
+        if (nullptr != father_ptr) {
+            father_ptr->set_child(child_ptr, type);
+        }
+
+        if (NodeType::ROOT == type) {
+            this->root_node = child_ptr;
+            if (nullptr != child_ptr) {
+                child_ptr->set_father(nullptr);
+                child_ptr->set_type(NodeType::ROOT);
+            }
+        } else if (nullptr != child_ptr) {
+            child_ptr->set_father(father_ptr);
+        }
+
+        if (NodeColor::RED == get_node_color(erase_ptr)) {
+            erase_fixup_red_black(child_ptr);
+        } else if (NodeColor::RED == get_node_color(child_ptr)) {
+            erase_fixup_red_black(child_ptr);
+        } else if (NodeType::ROOT == type) {
+            erase_fixup_root(child_ptr);
+        } else {
+            erase_fixup_black_black(father_ptr, type);
+        }
+        delete erase_ptr;
+    }
+
 private:
-    RbtreeNode<T> *root_node;
+    Node *root_node;
 };
 } // namespace frbtree
--- src/test.cpp
+++ src/test.cpp
@@ -1,21 +1,56 @@
+/*
+ * @Author: 469714875@qq.com 469714875@qq.com
+ * @Date: 2023-09-24 12:12:32
+ * @LastEditors: 469714875@qq.com 469714875@qq.com
+ * @LastEditTime: 2023-10-02 14:43:32
+ * @FilePath: \FiLib\src\test.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <iostream>
+#include <fstream>
+#include <set>
+#include <stdlib.h>
+#include <time.h>
 
 #include "magnum/frbtree.h"
 
 int main() {
     frbtree::Rbtree<int> rbtree;
-    std::cout << rbtree.find(1) << std::endl;
-    std::cout << rbtree.find(2) << std::endl;
-    std::cout << rbtree.find(3) << std::endl;
-    std::cout << rbtree.find(4) << std::endl;
-    std::cout << rbtree.find(5) << std::endl;
-    rbtree.push(4);
-    rbtree.push(2);
-    rbtree.push(5);
-    rbtree.push(1);
-    std::cout << rbtree.find(1) << std::endl;
-    std::cout << rbtree.find(2) << std::endl;
-    std::cout << rbtree.find(3) << std::endl;
-    std::cout << rbtree.find(4) << std::endl;
-    std::cout << rbtree.find(5) << std::endl;
+    std::set<int>        s;
+    constexpr int        len = 1000000;
+    constexpr int        max = 1000000;
+    srand((unsigned)time(NULL));
+    std::cout << "start insert" << std::endl;
+    for (int i = 0; i < len; i++) {
+        int x = rand() % max;
+        s.insert(x);
+        rbtree.push(x);
+    }
+    std::cout << "start find 1" << std::endl;
+    for (int i = 0; i < len; i++) {
+        int x               = rand() % max;
+        int set_find_res    = (s.find(x) != s.end());
+        int rbtree_find_res = rbtree.find(x);
+        if (set_find_res != rbtree_find_res) {
+            std::cout << "failed to find " << x << " in 1" << std::endl;
+            return 0;
+        }
+    }
+    std::cout << "start erase" << std::endl;
+    for (int i = 0; i < len; i++) {
+        int x = rand() % max;
+        s.erase(x);
+        rbtree.erase(x);
+    }
+    std::cout << "start find 2" << std::endl;
+    for (int i = 0; i < len; i++) {
+        int x               = rand() % max;
+        int set_find_res    = (s.find(x) != s.end());
+        int rbtree_find_res = rbtree.find(x);
+        if (set_find_res != rbtree_find_res) {
+            std::cout << "failed to find " << x << " in 2" << std::endl;
+            return 0;
+        }
+    }
+    std::cout << "success" << std::endl;
 }
